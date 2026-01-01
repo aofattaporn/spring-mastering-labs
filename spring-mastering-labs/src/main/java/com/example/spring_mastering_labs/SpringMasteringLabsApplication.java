b62e1b162a7927f8dd725a5b3ed52d19850630f8
s101472 <attaporn.peungsook@scb.co.th>
2026-01-01 20:50:02 +0700

    init project

---
+++ spring-mastering-labs/src/main/java/com/example/spring_mastering_labs/SpringMasteringLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mastering_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasteringLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasteringLabsApplication.class, args);
+	}
+
+}
---
+++ spring-mastering-labs/src/test/java/com/example/spring_mastering_labs/SpringMasteringLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mastering_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasteringLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
